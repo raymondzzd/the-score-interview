1b33b0981aa789e95f6f950ddbc633e5a0611f51
Raymond <raymondzzd@users.noreply.github.com>
2021-08-22 20:52:58 -0400

    fix data load

--- scripts/loadDataIntoDDB.js
+++ scripts/loadDataIntoDDB.js
@@ -1,46 +1,55 @@
 const AWS = require("aws-sdk");
 const fs = require('fs');
+const {v4: uuidv4} = require('uuid');
+
 async function main() {
-    AWS.config.update({
-        region: "us-east-1",
-        endpoint: "http://localhost:4566"
+  AWS.config.update({
+    region: "us-east-1",
+    endpoint: "http://localhost:4566"
+  });
+  const docClient = new AWS.DynamoDB.DocumentClient();
+  console.info("Script started...");
+  const filePath = `./rushing.json`;
+  const rawData = fs.readFileSync(filePath, 'utf-8');
+  let allPlayers = JSON.parse(rawData);
+  allPlayers.forEach(function (player) {
+    // console.debug("Processing: " + player.Player);
+    const params = setPutPayload(player);
+    docClient.put(params, function (err, data) {
+      if (err) {
+        console.error("Unable to add player: " + player.Player);
+        // console.debug("Error JSON: " + JSON.stringify(err));
+      } else {
+        // console.debug("PutItem succeeded: " + player.Player);
+      }
     });
-    const docClient = new AWS.DynamoDB.DocumentClient();
-    const filePath = `./rushing.json`;
-    const rawData = fs.readFileSync(filePath, 'utf-8');
-    let allPlayers = JSON.parse(rawData);
-    allPlayers.forEach(function (player) {
-        console.info("Processing: " + player.Player);
-        let params = {
-            TableName: "players",
-            Item: {
-                "Player": player.Player,
-                "Team": player.Team,
-                "Pos": player.Pos,
-                "Att": player.Att,
-                "AttG": player['Att/G'],
-                "Yds": player.Yds,
-                "Avg": player.Avg,
-                "YdsG": player['Yds/G'],
-                "TD": player.TD,
-                "Lng": player.Lng,
-                "First": player['1st'],
-                "FirstPercent": player['1st%'],
-                "TwentyPlus": player['20+'],
-                "FortyPlus": player['40+'],
-                "FUM": player.FUM,
-                "Status": player.Status
-            }
-        };
-        docClient.put(params, function (err, data) {
-            if (err) {
-                console.info("Unable to add player: " + player.Player);
-                console.error("Error JSON: " + JSON.stringify(err));
-            }
-            else {
-                console.info("PutItem succeeded: " + player.Player);
-            }
-        });
-    });
+  });
+  console.info("Script completed.");
 }
+
+function setPutPayload(player) {
+  return {
+    TableName: "players",
+    Item: {
+      "id": uuidv4(),
+      "Player": player.Player,
+      "Team": player.Team,
+      "Pos": player.Pos,
+      "Att": player.Att,
+      "AttG": player['Att/G'],
+      "Yds": player.Yds,
+      "Avg": player.Avg,
+      "YdsG": player['Yds/G'],
+      "TD": player.TD,
+      "Lng": player.Lng,
+      "First": player['1st'],
+      "FirstPercent": player['1st%'],
+      "TwentyPlus": player['20+'],
+      "FortyPlus": player['40+'],
+      "FUM": player.FUM,
+      "PlayerStatus": player.Status
+    }
+  };
+}
+
 main();
